cb7b23ed6644e4035aa15eb694678f3426af1b25
JensRas <58129125+JensRas@users.noreply.github.com>
2021-01-28 15:54:07 -0600

    Worked on checking sand

--- assignment0.c
+++ assignment0.c
@@ -18,20 +18,63 @@ int main(int argc, char *argv[])
   
   while(1)
   {
-  check_sand(pileArr);
-  print_map(pileArr);
+    pileArr[ARRAY_SIZE / 2][ARRAY_SIZE / 2] += 1;
+    check_sand(pileArr);
+    print_map(pileArr);
   }
   
   return 0;
 }
+/*
 //Function checks and topples piles of sand
 void check_sand(int pileArr[ARRAY_SIZE][ARRAY_SIZE])
 {
+  int i, j, l, m;
+  
   while(1){
+    for(j = 0; j < ARRAY_SIZE; j++) {
+      for (i = 0; i < ARRAY_SIZE; i++) {
+        if(pileArr[j][i] > 8) {
+          for(l = j - 1; l <= 1; l++)
+            pileArr[j][i] = 1;
+        }
+      }
+    }
     
   }
-    
 }
+*/
+// Recursive
+void rec_check(int pileArr[ARRAY_SIZE][ARRAY_SIZE]) 
+{
+  int j, i;
+
+  for (j = 0; j < 3; j++) {
+    for (i = 0; i < 3; i++) {
+      if (!(i == 2 && j == 2)) {
+        if (pileArr[j][i] < 9) {
+          break;
+        } 
+        else {
+          rec_check(helper(pileArr[j][i]));
+        }
+      }
+    }
+  }
+}
+
+// Helper
+int helper(int pileArr[ARRAY_SIZE][ARRAY_SIZE]) {
+  int j, i;
+  
+  for (j = 0; j < 3; j++) {
+    for (i = 0; i < 3; i++) {
+      pileArr[j][i] += 1;
+    }
+  }
+  return ;
+}
+
 //Function prints the map of sand piles
 void print_map(int pileArr[ARRAY_SIZE][ARRAY_SIZE])
 {
@@ -43,5 +86,4 @@ void print_map(int pileArr[ARRAY_SIZE][ARRAY_SIZE])
     }
     printf("\n");
   }
-  
 }
