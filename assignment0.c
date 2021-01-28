b57b33d9068a890fa175d257b8c36d52d85beb62
MythicAlchemist <thmsmccy@gmail.com>
2021-01-28 14:38:21 -0500

    Create assignment0.c
    base project file, subject to many changes

---
+++ assignment0.c
@@ -0,0 +1,48 @@
+/*
+@author Thomas McCoy & Jens Rasmussen
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#define ARRAY_SIZE = 23;
+
+//Function Headers
+void check_sand(int pileArr[ARRAY_SIZE][ARRAY_SIZE]);
+void print_map(int pileArr[ARRAY_SIZE][ARRAY_SIZE]);
+
+//Main
+int main(int argc, char *argv[])
+{
+  int pileArr[ARRAY_SIZE][ARRAY_SIZE];
+  
+  while(true do)
+  {
+  check_sand(pileArr);
+  print_map(pileArr);
+  }
+  
+  return 0;
+}
+//Function checks and topples piles of sand
+void check_sand(int pileArr[ARRAY_SIZE][ARRAY_SIZE])
+{
+  while(true do){
+    
+  }
+    
+}
+//Function prints the map of sand piles
+void print_map(int pileArr[ARRAY_SIZE][ARRAY_SIZE])
+{
+  int i, j;
+
+  for(j = 0; j < 10; j++){
+    for(i = 0; i < 10; i++){
+      printf("%d ", table[j][i]);
+    }
+    printf("\n");
+  }
+ 
+  
+}
