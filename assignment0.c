1671403aee7904d0f27e71d8a94d360302f45367
MythicAlchemist <thmsmccy@gmail.com>
2021-02-02 17:28:13 -0600

    Fixed main recursive functions

--- assignment0.c
+++ assignment0.c
@@ -12,21 +12,26 @@
 
 void print_map(int (*pileArr)[ARRAY_SIZE][ARRAY_SIZE]);
 void rec_check(int (*pileArr)[ARRAY_SIZE][ARRAY_SIZE], int col, int row);
-void helper(int (*pileArr)[ARRAY_SIZE][ARRAY_SIZE], int col, int row);
+void helper(int (**pileArr)[ARRAY_SIZE][ARRAY_SIZE], int col, int row);
+
+
 
 //Main
 int main(int argc, char *argv[])
 {
-  int pileArr[ARRAY_SIZE][ARRAY_SIZE]; 
+  int pileArr[ARRAY_SIZE][ARRAY_SIZE];
+  int f = 0;
+
   for(int i = 0; i < ARRAY_SIZE; i++){
     for(int j = 0; j < ARRAY_SIZE; j++){
       pileArr[i][j] = 0;
     }
   }
-  while(true){
-    pileArr[ARRAY_SIZE / 2][ARRAY_SIZE / 2] += 1;
+  while(f < 100){
+    (pileArr)[ARRAY_SIZE / 2][ARRAY_SIZE / 2] += 1;
     rec_check((&pileArr), ARRAY_SIZE / 2, ARRAY_SIZE / 2);
-    print_map(&pileArr);
+    print_map((&pileArr));
+    f++;
   }
   return 0;
 }
@@ -39,27 +44,29 @@ void rec_check(int (*pileArr)[ARRAY_SIZE][ARRAY_SIZE], int col, int row)
   for (j = -1; j <= 1; j++) {
     for (i = -1; i <= 1; i++) {
       if ((*pileArr)[j + col][i + row] > 8) {
+        helper((&pileArr), j + col, i + row);
         rec_check((pileArr), (j + col), (i + row));
+        
       }
     }
   }
-   helper((pileArr), j, i);
-   (*pileArr)[col][row] = 1;
+   
+   
 }
 
 // Helper
-void helper(int (*pileArr)[ARRAY_SIZE][ARRAY_SIZE], int col, int row)
+void helper(int (**pileArr)[ARRAY_SIZE][ARRAY_SIZE], int col, int row)
 {
   int j, i;
   
   for (j = -1; j <= 1; j++) {
     for (i = -1; i <= 1; i++) {
       if(!((j + col) < 0 || (j + col) > ARRAY_SIZE || (i + row) < 0 || (i + row) > ARRAY_SIZE)){
-	printf("%d \n", *pileArr[1][2]);
-	*pileArr[col + j][row + i] += 1;
+	      (**pileArr)[col + j][row + i] += 1;
       }
     }
   }
+  (**pileArr)[col][row] -= 9;
 }
 
 //Function prints the map of sand piles
